1cab7781687aed0583fdcf808a79557d6399cadf
Ben Wittenberg <benjaminwittenberg@gmail.com>
2024-05-15 22:26:03 -0400

    feat(ui): add ui package

--- apps/personal/src/components/Footer/Link.tsx
+++ apps/personal/src/components/Footer/Link.tsx
@@ -1,22 +1,6 @@
 import { ReactNode } from "react";
 import { anchor, container } from "./Link.css";
-
-export const ArrowIcon = () => {
-  return (
-    <svg
-      width="12"
-      height="12"
-      viewBox="0 0 12 12"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
-        fill="currentColor"
-      />
-    </svg>
-  );
-};
+import { Arrow } from "@bwittenberg/ui";
 
 type Props = {
   children: ReactNode;
@@ -26,7 +10,7 @@ type Props = {
 export const Link = ({ children, href }: Props) => {
   return (
     <div className={container}>
-      <ArrowIcon />
+      <Arrow />
       <a
         className={anchor}
         rel="noopener noreferrer"
